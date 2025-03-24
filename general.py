0c9abde541b8d5543ba74b46db1f926c01e2e74d
MacKenzie Jewell <kenzie.jewell7@gmail.com>
2025-03-23 17:36:29 -0700

    ocal

--- general.py
+++ general.py
@@ -25,3 +25,21 @@ def coriolis(latitude):
     f = 2 * omega * np.sin(np.deg2rad(latitude))
     
     return f
+
+
+
+def lat_weighted_mean(data_grid, latgrid):
+    
+    """ Calculate latitude-weighted spatial mean of data grid (for data on regular lat, lon grid)
+    INPUT:
+    - data_grid: (M x N) array of data values
+    - latgrid: (M x N) array of latitudes (degrees)
+    OUTPUT:
+    - weighted_mean: lat-weighted spatial mean of data_grid
+    """
+
+    lat_weights = np.cos(latgrid*np.pi/180)
+    
+    weighted_mean = np.sum(lat_weights*data_grid) / np.sum(lat_weights)
+    
+    return weighted_mean
